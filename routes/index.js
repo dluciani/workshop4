c79e779d95de07d688670a10356be3b0d8fad99b
Dan Luciani <dluciani@umass.edu>
2013-10-17 14:27:38 -0400

    added swap and check functions.

--- routes/index.js
+++ routes/index.js
@@ -50,3 +50,26 @@ exports.list = function(req, res)
 	}
 
 };
+
+var check = function(puzzle){
+	var solution = [0,1,2,3,4,5,6,7,8,9,10];
+	var correct = true;
+	for(var i = 0; i < solution.length; i++){
+		if(solution[i] == puzzle[i]){
+
+		}
+		else{
+			correct = false;
+			//NEED TO REDIRECT BACK TO INCORRECT PUZZLE???
+		}
+	}
+	return correct;
+
+};
+
+var swap = function(index1, index2, puzzle){
+	
+	var temp = puzzle[index1];
+	puzzle[index1] = puzzle[index2];
+	puzzle[index2] = temp;
+};
